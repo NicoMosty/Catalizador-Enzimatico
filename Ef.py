949d47e178e844bc5cad8564438b63143cf60d44
NicoMosty <npuentesu@unal.edu.co>
2022-01-22 01:31:22 -0500

    Adding plor for Efficnce Factor

--- Ef.py
+++ Ef.py
@@ -6,11 +6,14 @@ Created on Jan 19 2020
 from os import remove
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import axes3d
+from matplotlib import cm
+import math
 
 #___CONSTANTES__#
 dr=5e-4
-Beta=np.array([0.1,1,5])
-Phi=np.array([5,20,50])
+Beta=np.array([0.01,0.05,0.1,0.5,1,10])
+Phi=np.array([1,2,4,6,8,10,20,40,60,80,100])
 
 #_____GENERAR VECTOR DE RADIOS____#
 R=np.arange(0, 1+0.1*dr, dr)
@@ -71,7 +74,7 @@ def SvsR(phi,beta):
                 S[i]=S[i-1]+dr*dS[i-1]
                 dS[i]=dS[i-1]+dr*d2S[i-1]
                 d2S[i]=D2S(dS[i],S[i],R[i])
-        print("Beta={:5}|Phi={:5}|dSo={:7}|So={:9}|S[R]={:4}".format(beta,phi,dSo,So,S[R.size-1]))    
+        # print("Beta={:5}|Phi={:5}|dSo={:7}|So={:9}|S[R]={:4}".format(beta,phi,dSo,So,S[R.size-1]))    
         if S[R.size-1]>0.99:
             break
         else:         
@@ -86,15 +89,31 @@ def SvsR(phi,beta):
 SS=np.zeros((Phi.size,R.size))
 
 # Matriz de Eficiencias
-Ef=np.zeros((Phi.size,R.size))
+Ef=np.zeros((Beta.size,Phi.size))
 
-# Loop de Calculo de Concentraciones Cambiando Beta y Phi
+# Loop de Calculo de Concentraciones Cambiando Beta y Phi€
+print("______________________________")
 for B in Beta:
     for P in Phi:
         SvsR(P,B) 
         SS[list(Phi).index(P)]=St
         Ef[list(Beta).index(B),list(Phi).index(P)]=3*dSt/(P*P*B/(B+1))
-        
-        print("Beta={:5}|Phi={:5}|Ef={:5}".format(B,P,Ef[list(Beta).index(B),list(Phi).index(P)]))
-        print("OK")
-        print("__________________________________________")
+        print("Beta={:5}|Phi={:5}|Ef={:5.3f}".format(B,P,Ef[list(Beta).index(B),list(Phi).index(P)]))
+    print("______________________________")
+
+# Graficar SvsR 
+for B in Beta:
+    plt.plot(np.log(Phi), np.log(Ef[list(Beta).index(B)]), label= "Beta={}".format(B))
+plt.xlim(0,math.log(100))
+plt.ylim(math.log(0.1),math.log(1))
+plt.xlabel("Módulo de Thiele")
+plt.ylabel("Factor de Efectividad (μ)")
+
+PhiAxis=np.array([1,2,4,6,8,10,20,40,60,80,100])
+plt.xticks(np.log(PhiAxis),PhiAxis)
+EfAxis=np.array([0.1,0.2,0.4,0.6,0.8,1])
+plt.yticks(np.log(EfAxis),EfAxis)
+
+plt.legend(loc ="lower left")
+plt.savefig("Phi^Beta-vs-Ef.png")
+plt.show()
